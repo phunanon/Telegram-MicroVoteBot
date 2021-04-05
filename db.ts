7f76dc98556085d6c2d04e986075ae4ad6c4a1c2
phunanon <phunanon@users.noreply.github.com>
2021-04-05 16:58:13 +0100

    Before law voting introduced

--- db.ts
+++ db.ts
@@ -1,66 +1,150 @@
 import { exists } from "https://deno.land/std/fs/mod.ts";
 
-interface Vote {
-    TimeMs: number,
-    Choices: number,
+const readJson = async (path: string) => JSON.parse(await Deno.readTextFile(path));
+const writeJson = async (path: string, obj: any) => await Deno.writeTextFile(path, JSON.stringify(obj, null, 2));
+
+export interface Vote {
+    TimeSec: number,
+    Choices: number[],
 }
 
 export interface Ballot {
-    TimeMs: number,
-    ChatId: string,
+    TimeSec: number,
+    ChatId: number,
+    LawId: string,
     Minutes: number,
     Name: string,
     Desc: string,
     Options: string[],
+    Width: number,
     Votes: {[voterId: number]: Vote},
 }
 
-type Chat = {[userId2LastSeen: number]: number};
+export const instanceOfBallot = (obj: any): obj is Ballot => obj.hasOwnProperty("Votes");
 
-const readJson = async (path: string) => JSON.parse(await Deno.readTextFile(path));
-const writeJson = async (path: string, obj: any) => await Deno.writeTextFile(path, JSON.stringify(obj));
+export interface Law {
+    TimeSec: number,
+    Name: string,
+    Body: string,
+}
+
+export enum VoteStatus {
+    Success = "Vote cast successfully",
+    Unauthorised = "You must message on the chat this ballot was created, after its creation",
+    Expired = "This ballot expired",
+    Nonexist = "Ballot does not exist",
+    InvalidNumOptions = "Invalid number of options",
+}
+
+export interface Chat {
+    Name: string,
+    LastSeenSec: {[userId: number]: number},
+    Laws: Law[],
+}
+
+export const secNow = () => Math.floor(Date.now() / 1000);
+export const n2id = (n: number) => n.toString(36).toUpperCase();
+export const id2n = (id: string) => parseInt(id, 36);
+
+
+async function getChat (chatId: number):
+  Promise<{chat: Chat, write: ((chat: Chat) => Promise<void>)}> {
+    const fileName = `db/chat-${chatId}.json`;
+    if (!(await exists(fileName))) {
+        await writeJson(fileName, {Name: "", LastSeenSec: {}, laws: []});
+    }
+    return {
+        chat: await readJson(fileName) as Chat,
+        write: async (chat: Chat) => await writeJson(fileName, chat),
+    };
+}
+
+
+export async function getBallot (ballotId: string):
+  Promise<{ballot: Ballot | null, write: ((ballot: Ballot) => Promise<void>)}> {
+    const fileName = `db/ballot-${ballotId}.json`;
+    return {
+        ballot: await exists(fileName) ? await readJson(fileName) as Ballot : null,
+        write: async (ballot: Ballot) => await writeJson(fileName, ballot),
+    }
+}
+
 
 export async function newBallot(ballot: Ballot) {
-    const fileName = `db/ballot-${ballot.TimeMs}.json`;
+    const fileName = `db/ballot-${n2id(ballot.TimeSec)}.json`;
     await writeJson(fileName, ballot);
 }
 
-export async function castVote(ballotId: number, userId: number, vote: Vote): Promise<boolean> {
-    const fileName = `db/ballot-${ballotId}.json`;
-    const ballot = await readJson(fileName) as Ballot;
-    if (userCanVote(ballot, userId)) {
-        ballot.Votes[userId] = vote;
-        await writeJson(fileName, ballot);
-        return true;
-    } else {
-        return false;
+
+export async function castVote(ballotId: string, userId: number, choices: number[]):
+  Promise<VoteStatus | Ballot> {
+    const {ballot, write} = await getBallot(ballotId);
+    if (!ballot) {
+        return VoteStatus.Nonexist;
     }
+    if (!ballotIsOpen(ballot)) {
+        return VoteStatus.Expired;
+    }
+    if (!await userCanVote(ballot, userId)) {
+        return VoteStatus.Unauthorised;
+    }
+    if (choices.length != ballot.Options.length) {
+        return VoteStatus.InvalidNumOptions;
+    }
+    const vote: Vote = {
+        TimeSec: secNow(),
+        Choices: choices,
+    };
+    ballot.Votes[userId] = vote;
+    await write(ballot);
+    return ballot;
 }
 
-export async function logUser(chatId: number, userId: number) {
-    const fileName = `db/chat-${chatId}.json`;
-    if (!(await exists(fileName))) {
-        await writeJson(fileName, {});
-    }
-    const users = await readJson(fileName) as Chat;
-    users[userId] = Date.now();
-    await writeJson(fileName, users);
-}
 
-export async function getBallots(): Promise<Ballot[]> {
+export async function getBallots(chatId: number = 0): Promise<Ballot[]> {
     const files = [...Deno.readDirSync("./db")].filter(d => d.isFile && d.name.startsWith("ballot"));
-    return Promise.all(files.map(async f => await readJson(`db/${f.name}`) as Ballot));
+    const ballots = await Promise.all(files.map(async f => await readJson(`db/${f.name}`) as Ballot));
+    return chatId ? ballots.filter(b => b.ChatId == chatId): ballots;
 }
+
+
+export async function getUserBallots(userId: number): Promise<Ballot[]> {
+    const ballots = await getBallots();
+    const approvals = await Promise.all(ballots.map(async b => await userCanVote(b, userId)));
+    return ballots.filter((_, index) => approvals[index]);
+}
+
+
+export async function logUser(chatId: number, chatName: string, userId: number) {
+    const {chat, write} = await getChat(chatId);
+    chat.Name = chatName;
+    chat.LastSeenSec = {...chat.LastSeenSec, [userId]: secNow()};
+    await write(chat);
+}
+
+
+export const getLaws = async (chatId: number): Promise<Law[]> => (await getChat(chatId)).chat.Laws ?? [];
+
+
+export async function newLaw(chatId: number, law: Law) {
+    const {chat, write} = await getChat(chatId);
+    chat.Laws = [...chat.Laws ?? [], law];
+    write(chat);
+}
+
+
+export const ballotIsOpen = (ballot: Ballot): boolean => secNow() < ballot.TimeSec + (ballot.Minutes * 60);
+
 
 export async function userCanVote(ballot: Ballot, userId: number): Promise<boolean> {
     const fileName = `db/chat-${ballot.ChatId}.json`;
     if (!(await exists(fileName))) {
         return false;
     }
-    const users = (await readJson(fileName)) as Chat;
+    const {LastSeenSec: users} = (await readJson(fileName)) as Chat;
     if (!users[userId]) {
         return false;
     }
     const lastSeen = users[userId];
-    return lastSeen < ballot.TimeMs + (ballot.Minutes * 60000);
+    return lastSeen < ballot.TimeSec + (ballot.Minutes * 60);
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,46 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { Bot, Context } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
 import { State } from "https://deno.land/x/telegram@v0.1.1/context.ts";
-import { castVote, newBallot, logUser, Ballot, getBallots, userCanVote } from "./db.ts";
+import { castVote, newBallot, logUser, Ballot, getLaws, Law, newLaw, VoteStatus, getUserBallots, n2id, secNow, getBallot, instanceOfBallot, ballotIsOpen } from "./db.ts";
 
-async function handleBallot(input: string, ctx: Context<State>): Promise<string> {
-    const [period, name, desc, ...options] = input.split("\n");
+const ballotIdRegex = /^\[([A-Z0-9]+)\]/;
+const lawIdRegex = /\(?([A-Z0-9]+-[A-Z0-9]+)/;
+
+const repeat = (x: any, n: number): any[] => n < 1 ? [] : [...new Array(Math.floor(n))].map(_ => x);
+const repeatStr = (x: any, n: number): string => repeat(x, n).join("");
+const plural = (n: number, w: string) => `${n} ${w.replace("_", n != 1 ? "s" : "")}`;
+const drop1stLine = (lines: string): string => lines.split("\n").slice(1).join("\n");
+const showChoiceAmount = (n: number, width: number): string => unicodeHorizontalBar(width, n / width);
+const lawText = (l: Law, chatId: number) => `<code>(${n2id(chatId)}-${n2id(l.TimeSec)})</code> <b>${l.Name}</b>: ${l.Body}`;
+
+type ShowBallotOptions = {options?: boolean, desc?: boolean, amounts?: boolean}
+function ballotText (ballot: Ballot, show: ShowBallotOptions, amounts: number[] = []) {
+    const options = show.amounts
+        ? ballot.Options.map((o, i) => ({text: o, i, amount: amounts[i]}))
+                        .sort((a, b) => b.amount - a.amount)
+        : ballot.Options.map((o, i) => ({text: o, i, amount: i}));
+    return `<code>[${n2id(ballot.TimeSec)}]</code> <b>${ballot.Name}</b>` +
+        (show.desc ? `\n${ballot.Desc}` : "") +
+        (show.options ? options.map(opt =>
+            `\n<code>${show.amounts ? `${showChoiceAmount(opt.amount, ballot.Width)} ${opt.amount.toFixed(2)}` : ""} </code> ${opt.i+1}. ${opt.text}`).join("") : "");
+}
+
+const unicodeHorizontalBar = (width: number, fraction: number) => {
+    fraction = Math.min(1, Math.max(fraction, 0));
+    const wholeWidth = Math.floor(fraction * width);
+    const remainderWidth = (fraction * width) % 1;
+    const endWidth = width - wholeWidth - 1;
+    const partWidth = Math.floor(remainderWidth * 8);
+    const partChar = endWidth < 0 ? "" : [" ", "▏", "▎", "▍", "▌", "▋", "▊", "▉"][partWidth];
+    return repeatStr("▉", wholeWidth) + partChar + repeatStr(" ", endWidth);
+}
+
+
+const sendMessage = async (ctx: Context<State>, text: string) => await ctx.telegram.sendMessage({chat_id: ctx.chat?.id ?? 0, text, parse_mode: "HTML"});
+
+
+async function handleBallot(input: string, ctx: Ctx): Promise<string> {
+    let [period, name, desc, ...options] = input.split("\n");
     if (!period || !name || !desc || !options.length) {
         return await helpFor("ballot");
     }
@@ -14,97 +50,173 @@ async function handleBallot(input: string, ctx: Context<State>): Promise<string>
         const times: {[key: string]: number} = {m, h, d, M, y};
         return min + (parseInt(n) * (times[t] || 0));
     }, 0);
+    const lawIdRegex = /^\(.+?\)/;
     const ballot: Ballot = {
-        TimeMs: Date.now(),
-        ChatId: ctx.chat?.id.toString() ?? "",
+        TimeSec: secNow(),
+        ChatId: ctx.chatId,
+        LawId: name.match(lawIdRegex)?.groups?.$1 || "",
         Minutes: minutes,
-        Name: name,
+        Name: name.replace(lawIdRegex, "").trim(),
         Desc: desc,
         Options: options,
+        Width: 5,
         Votes: {},
     };
     newBallot(ballot);
-    return `New ballot: ${ballot.Name}\nYou must message in this chat prior to casting your vote, then use /mine in [this chat](https://t.me/MicroVoteBot).`;
+    return `${ballotText(ballot, {})}\nYou must message in this chat prior to casting your vote, then use /mine in <a href="https://t.me/MicroVoteBot">this chat</a>.`;
 }
 
-async function handleVote(input: string, voteMessage: string): Promise<string> {
-    if (voteMessage == "") {
-        return "Unknown error.";
+
+async function handleVote(input: string, ballotId: string, userId: number): Promise<string> {
+    const choices = input.split(" ");
+    const choiceNums = choices.map(s => parseInt(s));
+    if (choiceNums.includes(Number.NaN)) {
+        return "Please use only whole numbers to express your choice for a candidate.";
     }
-    return `${input} ${voteMessage}`;
+    const ballotOrStatus = await castVote(ballotId, userId, choiceNums);
+    if (instanceOfBallot(ballotOrStatus)) {
+        return `${ballotText(ballotOrStatus, {options: true, amounts: true}, choiceNums)}\n${VoteStatus.Success}.`;
+    }
+    return `${ballotOrStatus}.`;
 }
+
 
 async function handleResult(input: string): Promise<string> {
+    const ballotId = input.match(ballotIdRegex)?.[1];
+    const {ballot} = await getBallot(ballotId ?? "");
+    if (ballot == null) {
+        return "Ballot not found.";
+    }
+    if (ballotIsOpen(ballot) && false) {
+        return "This ballot is still open; results can only be provided once the ballot closes.";
+    }
+    const votes = Object.values(ballot.Votes);
+    const sums = votes.reduce((sums, v) => sums.map((s, i) => s + v.Choices[i]), ballot.Options.map(() => 0));
+    const avgs = sums.map(s => s / votes.length);
+    return `${ballotText(ballot, {options: true, desc: true, amounts: true}, avgs)}\n${plural(votes.length, "vote_")}`;
+}
+
+
+async function handleMine(input: string, ctx: Ctx): Promise<string> {
+    const ballots = await getUserBallots(ctx.userId);
+    if (!ballots.length) {
+        return "There are no ballots for you right now.";
+    }
+    ballots.forEach((b, i) => {
+        setTimeout(() => ctx.sendMessage(ballotText(b, {options: true})), (i + 1) * 100);
+    });
+    return `To cast a vote, reply to each ballot message with your choices such as: <code> 0 3 5 2</code>
+You can revote by doing the same again or replying to your own vote message.
+Ballots you can vote in now:`;
+}
+
+
+async function handleLaw(input: string) {
+    const lawId = input.match(lawIdRegex)?.[1];
     return "Erm";
 }
 
-async function handleMine(input: string, ctx: Context<State>): Promise<string> {
-    const ballots = await getBallots();
-    if (!ballots.length) {
-        return "No ballots available.";
+
+async function handleNewLaw(input: string, ctx: Ctx): Promise<string> {
+    const [name, ...body] = input.split("\n");
+    if (!name || !body.length) {
+        return await helpFor("law");
     }
-    const approvals = await Promise.all(ballots.map(async b => await userCanVote(b, ctx.message?.from?.id ?? 0)));
-    const okBallots = ballots.filter((_, index) => approvals[index]);
-    okBallots.forEach((b, i) => {
-        setTimeout(() => ctx.telegram.sendMessage({
-            chat_id: ctx.chat?.id ?? 0,
-            text: `[${b.TimeMs.toString(36).toUpperCase()}] Ballot: ${b.Name}`,
-        }), (i + 1) * 100);
-    });
-    return okBallots.length ? "Ballots you can vote in now:" : "There are no ballots for you right now.";
+    const law: Law = {
+        TimeSec: secNow(),
+        Name: name,
+        Body: body.join("\n"),
+    };
+    newLaw(ctx.chatId, law);
+    return lawText(law, ctx.chatId);
 }
+
+
+async function handleLaws(input: string, ctx: Ctx): Promise<string> {
+    const laws = await getLaws(ctx.chatId);
+    return laws.length ? laws.map(l => lawText(l, ctx.chatId)).join("\n-----\n") : "There are no laws for this chat.";
+}
+
+
+const getAllHelp = async () => (await Deno.readTextFile("help.txt")).split("\n\n");
+
+
+async function handleHelp() {
+    return (await getAllHelp()).map(h => `${h.split("\n")[0]}\n<pre>${drop1stLine(h)}</pre>`).join("\n");
+}
+
+
+async function handleStart() {
+    return "Welcome.";
+}
+
 
 async function helpFor(what: string): Promise<string> {
-    const helpFile = (await Deno.readTextFile("help.txt")).split("/");
-    const help = helpFile.find(h => h.startsWith(what));
-    return help ? `Usage of /${what}\`\`\`\n/${help}\`\`\`` : "No help found.";
+    const help = drop1stLine((await getAllHelp()).find(h => h.startsWith(`/${what}`)) ?? "")
+    return help ? `Usage of /${what}:\n<pre>${help}</pre>` : "No help found.";
 }
 
+
+type Ctx = {chatId: number, userId: number, sendMessage: (text: string) => void};
+type Command = {
+    test: RegExp,
+    handler: (input: string, ctx: Ctx) => Promise<string>,
+};
+
+const actions: Command[] = [
+    {test: /\/start/,     handler: handleStart},
+    {test: /\/newballot/, handler: handleBallot},
+    {test: /\/result/,    handler: handleResult},
+    {test: /\/mine/,      handler: handleMine},
+    {test: /\/newlaw\s/,  handler: handleNewLaw},
+    {test: /\/law\s/,     handler: handleLaw},
+    {test: /\/help/,      handler: handleHelp},
+];
+
 async function handleMessage (ctx: Context<State>) {
-    const text = ctx.message?.text ?? "";
+    if (ctx.chat == undefined || ctx.message == undefined || ctx.message.from == undefined || ctx.message.text == undefined) {
+        return;
+    }
+    const text = ctx.message.text;
     const input = text.replace(/.+?\s(.+)/, "$1");
+    const chatId = Math.abs(ctx.chat.id);
+    const chatName = ctx.chat.id == ctx.me.id ? "Myself" : ctx.chat.title ?? "Unknown";
+    const userId = ctx.message.from.id;
 
     //Log user activity in this chat for future authorisation
-    {
-        const chatId = ctx.chat?.id;
-        const from = ctx.message?.from;
-        if (chatId && from) {
-            logUser(chatId, from.id);
-        } else {
-            return;
-        }
-    }
+    logUser(chatId, chatName, userId);
 
     //Check if user is replying to one of our ballot messages (voting)
     if (ctx.message?.reply_to_message?.from?.id == (await ctx.telegram.getMe()).id) {
-        if (/^\[[A-Z0-9]+\] Ballot/.test(ctx.message.reply_to_message.text ?? "")) {
-            ctx.replyWithMarkdown(await handleVote(input, ctx.message?.reply_to_message?.text ?? ""));
+        const voteMessage = ctx.message.reply_to_message.text ?? "";
+        if (ballotIdRegex.test(voteMessage)) {
+            const ballotId = voteMessage.match(ballotIdRegex)?.[1];
+            if (ballotId) {
+                sendMessage(ctx, text == "/result"
+                    ? await handleResult(`[${ballotId}]`)
+                    : await handleVote(text, ballotId, ctx.message.from?.id ?? 0));
+            }
             return;
         }
     }
 
-    const actions: [RegExp, (input: string, ctx: Context<State>) => Promise<string>][] = [
-        [/\/ballot/, handleBallot],
-        [/\/result/, handleResult],
-        [/\/mine/, handleMine],
-    ];
-
-    const action = actions.find(([r]) => r.test(text))?.[1];
+    const action = actions.find(a => a.test.test(text))?.handler;
     if (!action) {
-        ctx.reply("No action found.");
         return;
     }
 
     const [what, help] = text.split(" ");
     if (help == "help") {
-        ctx.reply(await helpFor(what.slice(1)));
+        sendMessage(ctx, await helpFor(what.slice(1)));
         return;
     }
 
-    await ctx.replyWithMarkdown(await action(input, ctx));
+    await sendMessage(ctx, await action(input, {chatId, userId, sendMessage: (text: string) => sendMessage(ctx, text)}));
 }
+
 
 const token = config().BOT_TOKEN;
 const bot = new Bot(token);
+bot.telegram.setMyCommands([]);
 bot.on("text", handleMessage);
 bot.launch();
